af1bec7a9e45c03bd057198575bd1aa3159c4fb3
codeab <hackerjieson@gmail.com>
2015-12-10 17:22:35 +0800

    modify

--- media.py
+++ media.py
@@ -1,10 +1,39 @@
-import sys,ctypes
-from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QLabel,QProgressBar,QListWidget,
-	QSystemTrayIcon,QMenu,QAction,QGraphicsDropShadowEffect,QGraphicsBlurEffect,QListWidgetItem)
-from PyQt5.QtCore import Qt,QSize,QUrl,QThread
-from PyQt5.QtGui import QCursor,QIcon,QBrush,QColor,QDesktopServices 
-from conf.conf import conf
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
 
-class mywindow(QWidget):
+from PyQt5.QtMultimedia import ( QMediaPlayer, QMediaPlaylist,QMediaContent)
+
+
+
+
+class Media(object):
 	def __init__(self):
-		super().__init__()
+		self.player = QMediaPlayer()
+        self.playlist = QMediaPlaylist()
+        self.player.setPlaylist(self.playlist)
+    #播放
+	def play(self):
+		self.player.play()
+	def pause(self):
+		self.player.pause()
+	def main(self):
+		pass
+		# controls = PlayerControls()
+  #       controls.setState(self.player.state())
+  #       controls.setVolume(self.player.volume())
+  #       controls.setMuted(controls.isMuted())
+
+  #       controls.play.connect(self.player.play)
+  #       controls.pause.connect(self.player.pause)
+  #       controls.stop.connect(self.player.stop)
+  #       controls.next.connect(self.playlist.next)
+  #       controls.previous.connect(self.previousClicked)
+  #       controls.changeVolume.connect(self.player.setVolume)
+  #       controls.changeMuting.connect(self.player.setMuted)
+  #       controls.changeRate.connect(self.player.setPlaybackRate)
+  #       controls.stop.connect(self.videoWidget.update)
+
+
+
+
+
