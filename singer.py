509c72c81aa48681a042f908875d2b4b10dbb403
codeab <hackerjieson@gmail.com>
2015-12-25 16:40:01 +0800

    modify

--- singer.py
+++ singer.py
@@ -11,18 +11,12 @@ import re
 import os
 from json import *
 
-
 class bdmusic(object):
-
     def __init__(self):
         pass
-
-
     def search(self, key):
-        k = {'key': key}
-        url = "http://www.kuwo.cn/mingxing/"+key+"/pic.htm"
-        # url = "http://music.baidu.com/search?" + urllib.parse.urlencode(k)
-
+        k = {'k': key}
+        url = "http://www.kuwo.cn/mingxing/"+urllib.parse.urlencode(k)[2:]+"/pic.htm"
         user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0'
         req = urllib.request.Request(url)
         req.add_header('Referer', 'http://music.baidu.com/?from=new_mp3')
@@ -31,22 +25,14 @@ class bdmusic(object):
         the_page = response.read()
         # 关键字搜索结果
         res = the_page.decode("utf8")
-        print(res) 
-        # {"songItem":{"sid":121223583,"author":"<em>\u5f20\u6770<\/em>","sname":"\u5251\u5fc3","oid":121223583,"pay_type":"0","isJump":0}}
-        # {&quot;sid&quot;:14944589,&quot;author&quot;:&quot;&lt;em&gt;\u5f20\u6770&lt;\/em&gt;&quot;,&quot;sname&quot;:&quot;\u9006\u6218&quot;,&quot;oid&quot;:14944589,&quot;pay_type&quot;:&quot;0&quot;,&quot;isJump&quot;:0}}
-        # pattern = re.compile(r'sid&quot;:\d+')
-        # s = pattern.findall(res)
-        # songlist = []
-        # for songid in s:
-        #     songlist.append(str(songid).split(':')[1])
-        # # 返回关键字搜索结果最多20条  歌曲的 songId
-        # # print(songlist)
-        # return songlist
+        pattern = re.compile(r'<dd>(.*)<\/dd>')
+        s = pattern.findall(res)
+        print(s)
 
 
 if __name__ == '__main__':
     q = bdmusic()
-    data = q.search("张杰".encode('utf-8'))
+    data = q.search("张杰")
     # key = u"张杰"
     # print(key)
     # print(data)
