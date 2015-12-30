5d13f2326b40a6a6dbc2d07f02b8b07ea8a59a71
codeab <hackerjieson@gmail.com>
2015-12-30 13:37:23 +0800

    test

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import urllib.parse
 import urllib.request
 import re
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QPushButton, QLineEdit, QLabel)
+    QApplication, QWidget, QPushButton, QLineEdit, QLabel,QListWidget,QListWidgetItem)
 from PyQt5.QtWebKitWidgets import QWebPage, QWebView
 from PyQt5.QtCore import Qt, QUrl, pyqtSlot,QTimer
 
@@ -18,54 +18,44 @@ class Example(QWidget):
         self.initUI()
         
         
-    def initUI(self):      
+    def initUI(self):     
+        s = """
+        QScrollBar:vertical {
+    border: 2px solid grey;
+    background: #32CC99;
 
-        self.w= QWidget(self)
-        self.setGeometry(300,100,1000,600)
-        self.web = QWebView(self)
-        self.web.loadFinished.connect(self.test)
-        
-        self.web.page().setLinkDelegationPolicy(QWebPage.DelegateAllLinks)
-        self.web.page().linkClicked.connect(self.linkClicked)
 
-        self.web.setGeometry(0, 0, 1000, 500)
-        self.btn = QPushButton("测试",self);
-        self.btn.clicked.connect(self.test)
-        self.btn.move(300,550)
-        self.web.load(QUrl("http://image.baidu.com/"))
+}
+QScrollBar::handle:vertical {
+    background: white;
+    min-width: 20px;
+}
+QScrollBar::add-line:vertical {
+    border: 2px solid grey;
+    background: #32CC99;
+    width: 20px;
+    subcontrol-position: right;
+    subcontrol-origin: margin;
+}
+
+QScrollBar::sub-line:vertical {
+    border: 2px solid grey;
+    background: #32CC99;
+    width: 20px;
+    subcontrol-position: left;
+    subcontrol-origin: margin;
+}
+        """ 
+        self.setStyleSheet(s)
+        #列表
+        songList = QListWidget(self)
+        songList.setGeometry(2,0,238,370)  
+        for x in range(100):
+            item = QListWidgetItem("hucida cdsa%d" % (x))
+            songList.addItem(item)
+        # songList.setStyleSheet(qss_songlist)
         self.show()
-    def test(self):
-        print("jiazaijieshu")
-        frame = self.web.page().currentFrame()
-        searchinput = frame.findFirstElement('#kw')
-        searchinput.setAttribute("value","张杰")
-        searchinput.setAttribute("readonly","readonly")
-    def linkClicked(self,url):
-        # print(url.toString())
-        url = url.toString()
-        pattern = re.compile(r'&word=(.*?)&')
-        s = pattern.findall(url)
-        k = {'word': s[0]}
-        kv = urllib.parse.urlencode(k)
-        url = url.replace("word="+s[0], kv)
-
-        res = urllib.request.urlopen(url).read().decode("utf8")
-        pattern = re.compile(r'currentImg(.*)<div>',re.S)
-        s = pattern.findall(res)
-        src="http://img3.imgtn.bdimg.com/it/u=673176467,634723054&amp;fm=21&amp;gp=0.jpg"
-        pattern = re.compile(r'src="(.*?)"')
-        s = pattern.findall(s[0])
-        img_url = s[0].replace("&amp;","&")
-
-        local = os.path.join('./cache/', '张杰.jpg')
-        user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0'
-        req = urllib.request.Request(img_url)
-        req.add_header('Referer', 'http://music.baidu.com/?from=new_mp3')
-        req.add_header('User-Agent', user_agent)
-        f = urllib.request.urlopen(req)
-        data = f.read() 
-        with open(local, "wb") as code:     
-            code.write(data)
+        
 
 
 
